c4a6caf14e60fd2774d37b668f1aef0eda8b9a9f
Larry Burris <lburris@mac.com>
2021-07-02 21:56:28 -0400

    Initial Commit

---
+++ LogUtil/LogUtilApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogUtilApp.swift
+//  LogUtil
+//
+//  Created by Larry Burris on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LogUtilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
